ca2dcafcda06e7fcee187de4a7a69c41540d5882
rohansadhukhan65 <sadhukhanrohan15@gmail.com>
2021-06-28 11:10:55 +0530

    thisrd commit

--- main/application/config/config.php
+++ main/application/config/config.php
@@ -23,9 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codeigniter/TestYourSkill/main/';
 
-// $config['base_url'] = 'http://localhost/TestYourSkill_codeigniter_project/main/';
+// Rohan's System path
+// $config['base_url'] = 'http://localhost/codeigniter/TestYourSkill/main/';   
+
+//Sneha's System path
+// $config['base_url'] = 'http://localhost/TestYourSkill_codeigniter_project/main/'; 
+
 /*
 |--------------------------------------------------------------------------
 | Index File

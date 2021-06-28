199b8d6ce9d0383bb416fa7e37c855962e29380c
rohansadhukhan65 <sadhukhanrohan15@gmail.com>
2021-06-28 11:01:57 +0530

    thisrd commit

--- main/application/config/config.php
+++ main/application/config/config.php
@@ -25,6 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/codeigniter/TestYourSkill/main/';
 
+// $config['base_url'] = 'http://localhost/TestYourSkill_codeigniter_project/main/';
 /*
 |--------------------------------------------------------------------------
 | Index File
